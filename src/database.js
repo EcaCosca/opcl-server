365cab760e1551ecf7015bf59386b4521cc6f983
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2021-07-05 17:27:30 +0200

    refactored code from database.js

--- src/database.js
+++ src/database.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 
-const URI = 'mongodb://localhost/mernstack';
+const URI = 'mongodb://localhost/opcl';
 
 mongoose.connect(URI, {
-    useNewUrlParser: true;
+    useNewUrlParser: true,
+    useCreateIndex: true
 })
+
+const connection = mongoose.connection;
