484a2caccd14eddb89df2d35ec79d6eac4d50355
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2021-07-06 12:58:50 +0200

    created controller files for users and products in the controllers folder

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -8,6 +8,7 @@ router.route('/')
 
 
 router.route('./:id')
-    .get()
+    .get((req, res) => res.json({message: 'GET request'}))
+
 
 module.exports = router;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,4 +5,9 @@ router.route('/')
     .get((req, res) => res.json({message: 'GET request'}))
     .post((req, res) => res.json({message: 'POST request'}));
 
-module.exports = router;
+router.route('/')
+    .get((req, res) => res.json({message: 'GET user request'}))
+    .put((req, res) => res.json({message: 'PUT user request'}))
+    .delete((req, res) => res.json({message: 'DELETE user request'}));
+
+    module.exports = router;
