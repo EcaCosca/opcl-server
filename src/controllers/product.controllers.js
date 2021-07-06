0a253f1a2797ef33d84779f64e1f207e62babe75
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2021-07-06 17:22:19 +0200

    Conected product.model to product controller

--- src/controllers/product.controllers.js
+++ src/controllers/product.controllers.js
@@ -1,5 +1,7 @@
 const productCtrl = {};
 
+const Product = require('../models/product.model');
+
 productCtrl.getProducts = (req, res) => res.json({message: []});
 
 productCtrl.getProduct = (req, res) => res.json({message: 'GET request'})
