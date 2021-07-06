6f15c0d5dd71204c80f4b7906a8730587d2cf71f
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2021-07-06 11:45:42 +0200

    conected the server, created the first few routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+// SETTINGS
+app.set('port', process.env.PORT || 4000);
 
+
+// MIDDLEWARES
+app.use(cors());
+app.use(express.json());
+
+// ROUTES 
+app.get('/api/users', (req, res) => res.send('User routes')) 
+app.get('/api/notes', (req, res) => res.send('Notes routes')) 
 
 module.exports = app;
--- src/database.js
+++ src/database.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
-const URI = 'mongodb://localhost/opcl';
+const URI = process.env.MONGODB_URI ? 'process.env.MONGODB_URI' : 'mongodb://localhost/databasetest';
 
 mongoose.connect(URI, {
     useNewUrlParser: true,
-    useCreateIndex: true
+    useCreateIndex: true,
+    useUnifiedTopology: true
 })
 
 const connection = mongoose.connection;
