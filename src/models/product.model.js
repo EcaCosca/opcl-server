eb3cf39fbe3f10ebfc7d6b61f9e9e9427ea7d4c7
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2021-07-06 15:15:56 +0200

    Filled categories for product model

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,12 +1,18 @@
 const { Schema, model } = require('mongoose');
 
 const productSchema = new Schema ({
-    title: String,
-    content: {
-        type: String,
-        required: true
-    },
-    author: String
+    category: {type: String},
+    series: {type: String},
+    model: {type: String, unique: true},
+    descripcion: { type: String},
+    imgProducto: {type: String},
+    imgTecnica: {type: String},
+    manualInstructivo: {type: String},
+    dimensiones: {
+        ancho: {type: Number},
+        largo: {type: Number},
+        alto: {type: Number},
+    }
 }, {
     timestamps: true
 });
