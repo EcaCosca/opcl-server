011af0dad0036f5eaeb79000d94ff6ae3331c063
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2021-07-06 17:29:28 +0200

    refactored the get request on product.controllers to get a list of products inside the database

--- src/controllers/product.controllers.js
+++ src/controllers/product.controllers.js
@@ -2,9 +2,14 @@ const productCtrl = {};
 
 const Product = require('../models/product.model');
 
-productCtrl.getProducts = (req, res) => res.json({message: []});
+productCtrl.getProducts = async (req, res) => {
+    const products = await Product.find();
+    res.json(products);
+};
 
-productCtrl.getProduct = (req, res) => res.json({message: 'GET request'})
+productCtrl.getProduct = (req, res) => {
+    res.json({message: 'GET request'})
+};
 
 
 module.exports = productCtrl;
