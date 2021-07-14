7edd8a7813a8cdc0031379eb9bcae90799182b54
Enrique Coscarelli <enriquecoscarelli@gmail.com>
2021-07-14 13:50:08 +0200

    Changed model and route details

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,13 +1,34 @@
 const { Schema, model } = require('mongoose');
 
 const productSchema = new Schema ({
-    category: {type: String},
-    series: {type: String},
-    model: {type: String, unique: true},
-    descripcion: { type: String},
-    imgProducto: {type: String},
-    imgTecnica: {type: String},
-    manualInstructivo: {type: String},
+    codSerie: {type: String},
+    codFuncion: {type: String},
+    status: {type: String},
+    ANYO_CERT: {type: String},
+    certificado: {type: String},
+    certificado2: {type: String},
+    certificado3: {type: String},
+    esquemaTippo: {type: String},
+    logoCE: {type: String},
+    normativa: {type: String},
+    obsoleto: {type: String},
+    certificadoCE: {type: String},
+    cajaDimensiones: {type: String},
+    durabilidad: {type: String},
+    fuerzaCierreNewton: {type: String},
+    material: {type: String},
+    resitenciaCierre: {type: String},
+    resistenciaFuego: {type: String},
+    resistenciaIP: {type: String},
+    temperatura: {type: String}
+
+    // category: {type: String},
+    // series: {type: String},
+    // model: {type: String, unique: true},
+    // descripcion: { type: String},
+    // imgProducto: {type: String},
+    // imgTecnica: {type: String},
+    // manualInstructivo: {type: String},
     // dimensiones: {
     //     ancho: {type: Number},
     //     largo: {type: Number},
@@ -15,9 +36,10 @@ const productSchema = new Schema ({
     // }
 
     // Changing the entire schema for google sheets JSON file
+
 }, {
     timestamps: true
 });
 
 
-module.exports =model('Product', productSchema);
+module.exports = model('Product', productSchema);
